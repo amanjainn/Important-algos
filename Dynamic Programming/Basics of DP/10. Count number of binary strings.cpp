b16f5ef880a8951fcb044adbfe29012b09dd074d
Aman <amangrand@gmail.com>
2020-07-11 13:02:51 +0530

    Count number of binary strings.cpp

---
+++ Dynamic Programming/Basics of DP/10. Count number of binary strings.cpp
@@ -0,0 +1,44 @@
+
+
+
+/*
+Name : Aman Jain
+Date : 11-07-2020
+
+
+Given a positive integer N, count all possible distinct binary strings of length N such that there are no consecutive 1’s. 
+Time Complexity :O(logn)
+
+*/
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+ll binaryStrings(ll n, ll dp[]){
+    if(n<1)
+    return 0;
+    if(n==1)
+    return 2;
+    if(n==2)
+    return 3;
+   
+    if(dp[n]!=0)
+    return dp[n];
+
+    dp[n]=binaryStrings(n-1,dp)+ binaryStrings(n-2,dp);
+  return dp[n];
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        ll dp[1000]={0};
+        cout<<binaryStrings(n,dp)<<endl;
+    }
+}
