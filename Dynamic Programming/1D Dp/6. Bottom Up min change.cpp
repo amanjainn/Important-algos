ed5a090a54a13f84405dd8c5e9288f7831cf542f
Aman Jain <amangrand@gmail.com>
2020-08-15 15:52:08 +0530

    Update 6. Bottom Up min change.cpp

--- Dynamic Programming/1D Dp/6. Bottom Up min change.cpp
+++ Dynamic Programming/1D Dp/6. Bottom Up min change.cpp
@@ -10,28 +10,38 @@ Space Complexity : O(n)
 #include<bits/stdc++.h>
 using namespace std;
 
-int minCoins(int dp[],int coins[],int t, int n){
-    for(int i=1; i <=n ; i++){
-        int subprob=INT_MAX;
-        for(int j=0 ; j<t; j++){
-            if(i-coins[j]>=0)
-               subprob=min(subprob,dp[i-coins[j]]+1);
-        }
-      
-    dp[i]=subprob;
-    }
-    return dp[n];
-}
+
 int main(){
-    int n,t;
-    cin>>n>>t;
-    int coins[t];
-    for(int i=0 ; i<t; i++){
+    int n,p;
+    cin>>n>>p;
+    int coins[p];
+    for(int i=0; i<p ;i++){
         cin>>coins[i];
     }
-    int dp[100]={0};
-    cout<<minCoins(dp,coins,t,n);
+    sort(coins,coins+p);
+    int dp[1000001]={0};
+    for(int i=1; i<=100000 ;i++){
+        dp[i]=INT_MAX;
+    }
+    int ans=0;
+    for(int i=1; i<=n ; i++){
+        ans=0;
+        for(int j=0 ; j<p ; j++){
+            if(i-coins[j]>=0){
+                ans=dp[i-coins[j]];
+                if(ans!=INT_MAX){
+                    ans=ans+1;
+                }
+                dp[i]=min(ans,dp[i]);
+            }
+        }  
+    }
+    if(dp[n]!=INT_MAX)
+    cout<<dp[n]<<endl;
+    else{
+        cout<<"-1"<<endl;
+    }
+  }
 }
-
 
 
